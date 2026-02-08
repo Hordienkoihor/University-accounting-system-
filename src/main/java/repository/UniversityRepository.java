3f0f4e1a93e844e2b89563bb0c0f0863f575590d
Hordienkoihor <hordienkoihor05@gmail.com>
2026-02-08 15:58:25 +0200

    added student and faculty loading from the file(date needs further look, jam set for now:

--- src/main/java/repository/UniversityRepository.java
+++ src/main/java/repository/UniversityRepository.java
@@ -1,11 +1,16 @@
 package repository;
 
+import domain.Faculty;
+import domain.Student;
 import domain.University;
+import domain.enums.StudyForm;
+import domain.enums.StudyStatus;
 import repository.interfaces.UniversityRepositoryInt;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
 
 public class UniversityRepository implements UniversityRepositoryInt {
@@ -32,13 +37,97 @@ public class UniversityRepository implements UniversityRepositoryInt {
 
         try {
             List<String> lines = Files.readAllLines(Paths.get(path));
+            boolean loadedUniversity = false;
             for (String line : lines) {
                 String[] parts = line.split(",");
 
+
                 switch (parts[0]) {
-                    case "UNIVERSITY":
-                        result = new University(parts[1], parts[2], parts[3], parts[4]);
+                    case "UNIVERSITY": {
+                        if (!loadedUniversity) {
+                            result = new University(parts[1], parts[2], parts[3], parts[4]);
+                            loadedUniversity = true;
+                        }
+
                         break;
+                    }
+                    case "FACULTY": {
+                        if (loadedUniversity) {
+                            if (parts.length >= 3) {
+                                result.addFaculty(new Faculty(parts[1], parts[2]));
+                            }
+                        }
+                        break;
+                    }
+                    case "STUDENT": {
+                        if (loadedUniversity) {
+                            if (parts.length < 6) {
+                                break;
+                            }
+
+                            if (parts.length < 8) {
+                                StudyForm studyForm = parts[7].equalsIgnoreCase(StudyForm.TUITION.getDisplayName())
+                                        ? StudyForm.TUITION
+                                        : StudyForm.TUITION_FREE;
+
+
+                                result.addStudent(new Student(
+                                        parts[1],
+                                        parts[2],
+                                        parts[3],
+                                        Integer.parseInt(parts[4]),
+                                        parts[5],
+                                        parts[6],
+                                        new Date(),
+                                        studyForm,
+                                        StudyStatus.PENDING));
+
+                            } else {
+                                StudyStatus studyStatus = StudyStatus.PENDING;
+
+                                StudyForm studyForm = parts[7].equalsIgnoreCase(StudyForm.TUITION.getDisplayName())
+                                        ? StudyForm.TUITION
+                                        : StudyForm.TUITION_FREE;
+
+                                switch (parts[8].toUpperCase()) {
+                                    case "STUDYING": {
+                                        studyStatus = StudyStatus.STUDYING;
+                                        break;
+                                    }
+                                    case "EXPELLED": {
+                                        studyStatus = StudyStatus.EXPELLED;
+                                        break;
+                                    }
+                                    case "ACADEMIC_LEAVE": {
+                                        studyStatus = StudyStatus.ACADEMIC_LEAVE;
+                                        break;
+                                    }
+                                    case "PENDING": {
+                                        studyStatus = StudyStatus.PENDING;
+                                        break;
+                                    }
+                                    default: {
+                                        break;
+                                    }
+
+
+                                }
+
+                                result.addStudent(new Student(
+                                        parts[1],
+                                        parts[2],
+                                        parts[3],
+                                        Integer.parseInt(parts[4]),
+                                        parts[5],
+                                        parts[6],
+                                        new Date(),
+                                        studyForm,
+                                        studyStatus
+                                ));
+                            }
+                        }
+                        break;
+                    }
                     default:
                         break;
                 }
