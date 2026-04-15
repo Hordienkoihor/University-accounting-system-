905c5aa42fad12b65f7b1ea3d6fbbb66706b1b6f
Hordienkoihor <hordienkoihor05@gmail.com>
2026-04-15 14:55:57 +0300

    chore: DepartmentRepository logging. fix: fix initializing

--- src/main/java/repository/DepartmentRepository.java
+++ src/main/java/repository/DepartmentRepository.java
@@ -2,6 +2,8 @@ package repository;
 
 import domain.Department;
 import domain.Faculty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import repository.interfaces.DepartmentRepositoryInt;
 import repository.io.PersistenceService;
 
@@ -12,14 +14,19 @@ public class DepartmentRepository implements DepartmentRepositoryInt {
     private final Map<String, Department> departments = new ConcurrentHashMap<>();
     private final PersistenceService<Department> persistence = new PersistenceService<>(Department.class, "departments.json");
 
+    private final Logger log = LoggerFactory.getLogger(DepartmentRepository.class);
 
     public DepartmentRepository() {
-        persistence.loadAll().forEach(this::save);
+        log.info("Initializing DepartmentRepository");
+        List<Department> loadedData = persistence.loadAll();
+        loadedData.forEach(d -> departments.put(d.getCode(), d));
+        log.info("Initialized with {} departments", departments.size());
     }
 
     @Override
     public void save(Department entity) {
         departments.put(entity.getCode(), entity);
+        log.debug("Department with code {} saved to memory", entity.getCode());
         persistence.saveAll(findAll());
     }
 
@@ -40,7 +47,11 @@ public class DepartmentRepository implements DepartmentRepositoryInt {
 
     @Override
     public void deleteById(String s) {
-        Department department = departments.remove(s);
-        persistence.saveAll(findAll());
+        if (departments.remove(s) != null) {
+            log.info("Department with code {} deleted", s);
+            persistence.saveAll(findAll());
+        } else {
+            log.warn("Attempted to delete non-existent department with code: {}", s);
+        }
     }
 }
