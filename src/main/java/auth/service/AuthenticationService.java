415056923ece490830e93acdb77e10c468bfff1c
Hordienkoihor <hordienkoihor05@gmail.com>
2026-04-22 20:29:52 +0300

    chore: add auth services logging

--- src/main/java/auth/service/AuthenticationService.java
+++ src/main/java/auth/service/AuthenticationService.java
@@ -5,6 +5,8 @@ import auth.entities.LoginResponse;
 import auth.entities.User;
 import auth.repository.interfaces.UserRepositoryInt;
 import auth.service.interfaces.AuthenticationServiceInt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -14,8 +16,12 @@ public class AuthenticationService<T extends User> implements AuthenticationServ
     private final UserRepositoryInt<T> userRepository;
     private final Set<Integer> activeTokens = new HashSet<>();
 
+    private final Logger log = LoggerFactory.getLogger(AuthenticationService.class);
+
     public AuthenticationService(UserRepositoryInt<T> userRepository) {
+        log.info("Initializing AuthenticationService");
         this.userRepository = userRepository;
+        log.info("Initialized AuthenticationService");
     }
 
     @Override
@@ -23,30 +29,36 @@ public class AuthenticationService<T extends User> implements AuthenticationServ
         Optional<T> user = userRepository.findByNameAndPassword(name, password);
 
         if (user.isEmpty()) {
+            log.error("User with login: {} and password: {}  not found", name, password);
             throw new RuntimeException("Invalid username or password");
         }
 
         int token = generateToken(user.get());
 
         if (activeTokens.contains(token)) {
+            log.error("User already logged in: {}", user.get());
             throw new RuntimeException("User already logged in");
         }
 
         activeTokens.add(token);
 
+        log.info("User logged in: {}", user.get());
         return new LoginResponse<>(user.get(), token);
     }
 
     @Override
     public T register(T user) {
         if (user instanceof HeadAdminRole) {
+            log.error("Head admin role is not supported");
             throw new RuntimeException("Cannot register head admin role");
         }
 
         if (userRepository.existsById(user.getName())) {
+            log.error("User with login: {} already registered", user.getName());
             throw new RuntimeException("User already exists");
         }
         userRepository.save(user);
+        log.info("User registered: {}", user);
         return user;
     }
 
@@ -55,10 +67,12 @@ public class AuthenticationService<T extends User> implements AuthenticationServ
         int token = generateToken(user);
 
         if (!activeTokens.contains(token)) {
+            log.error("User is not logged in: {}", user);
             throw new RuntimeException("User is not currently logged in");
         }
 
         activeTokens.remove(token);
+        log.info("User logged out: {}", user);
     }
 
     @Override
--- src/main/java/auth/service/AuthorizationService.java
+++ src/main/java/auth/service/AuthorizationService.java
@@ -4,18 +4,25 @@ import auth.entities.User;
 import auth.enums.Right;
 import auth.service.interfaces.AuthenticationServiceInt;
 import auth.service.interfaces.AuthorizationServiceInt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 
 public class AuthorizationService implements AuthorizationServiceInt {
     private AuthenticationServiceInt authenticationService;
 
+    private final Logger log = LoggerFactory.getLogger(AuthorizationService.class);
+
     public AuthorizationService(AuthenticationServiceInt authenticationService) {
+        log.info("Initializing AuthorizationService");
         this.authenticationService = authenticationService;
+        log.info("Initialized AuthorizationService");
     }
 
     @Override
     public int provideAuthorityMask(User user) {
+        log.info("Provided rights mask for {}", user.getName());
         return user.getRightsMask();
     }
 
@@ -37,11 +44,14 @@ public class AuthorizationService implements AuthorizationServiceInt {
     @Override
     public void setRight(User user, Right right) {
         user.setRightsMask(Right.addRight(user.getRightsMask(), right));
+        log.info("Right {} set to {}", right.name(), user.getName());
     }
 
     @Override
     public void revokeRight(User user, Right right) {
         user.setRightsMask(Right.removeRight(user.getRightsMask(), right));
+        log.info("Right {} revoked from {}", right.name(), user.getName());
+
     }
 
 //    @Override
